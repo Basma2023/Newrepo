6f6c9fc260335187a456fc35f8826d7f92cd3fcc
Basma2023 <122500710+Basma2023@users.noreply.github.com>
2023-03-27 14:55:52 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-1_JbA2wl58h", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "32iok7lmqgvf1b0plts731a0fd", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "us-east-1_69EJ69nTT", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "eqhj80s1o6g08iscfkhncp2a1", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "us-east-1", // e.g. us-east-2
   },
   api: {
