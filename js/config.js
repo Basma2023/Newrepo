da4dd9634c9382cf758ec328e3d8aa6e65cefd12
Basma2023 <122500710+Basma2023@users.noreply.github.com>
2023-03-27 16:00:12 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,19 @@
 window._config = {
-  cognito: {
-    userPoolId: "us-east-1_69EJ69nTT", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "eqhj80s1o6g08iscfkhncp2a1", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "us-east-1", // e.g. us-east-2
-  },
-  api: {
-    invokeUrl: "https://main.d3c16r36tjrrmp.amplifyapp.com/", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-  },
+
+    cognito: {
+
+        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb         
+
+        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'us-west-2' // e.g. us-east-2 
+
+    }, 
+
+    api: { 
+
+        invokeUrl: 'https://xgwznv7yy3.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
 };
